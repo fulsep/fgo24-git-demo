6450d78d76c50833613e9f9fe8c7d5b952a6b120
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-25 17:12:44 +0700

    feat: add feature for printing triangle based on user input

---
+++ src/triangle.js
@@ -0,0 +1,18 @@
+/**
+ * print triangle whose area based on input
+ * @param {number} input 
+ */
+
+function triangle(input) {
+    for (let i = 1; i <= input; i++) {
+        let margin = ""
+        for (let m = input; m > i; m--) {
+            margin += " "
+        }
+        let star = ""
+        for (let s = 0; s < i; s++) {
+            star += "* "
+        }
+        console.log(margin + star)
+    }
+}
